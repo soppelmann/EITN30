41e67937ae0265cf969c8bf37d413b0d9fbdc7d7
rpi25 <rpi25@eitn30.se>
2024-03-29 16:00:03 +0000

    rxloop examples, cant get it working due to PEBKAC

--- src/bin/rxloop.rs
+++ src/bin/rxloop.rs
@@ -1,7 +1,7 @@
 //use std::io;
 
 use embedded_hal::delay::DelayNs;
-use embedded_nrf24l01::{Configuration, CrcMode, DataRate, NRF24L01};
+use embedded_nrf24l01::{Configuration, CrcMode, DataRate, NRF24L01, PIPES_COUNT, Payload, RxMode, Device};
 
 use linux_embedded_hal::spidev::{self, SpidevOptions};
 //use linux_embedded_hal::sysfs_gpio::Direction;
@@ -25,7 +25,7 @@ fn main() {
 
     // Configure CE pin
     let mut chip = Chip::new("/dev/gpiochip4").unwrap();
-    let handle = chip.get_line(17).unwrap(); // Get LineHandle using pin number
+    let handle = chip.get_line(27).unwrap(); // Get LineHandle using pin number
     let req_flags = LineRequestFlags::OUTPUT;
     let ce = match CdevPin::new(handle.request(req_flags, 0, "my_ce_pin").unwrap()) {
         Ok(pin) => pin,
@@ -40,47 +40,94 @@ fn main() {
     // https://github.com/astro/embedded-nrf24l01/issues/12
     //let mut nrf24 = nrf24.tx().unwrap(); //default configuration from example
 
-    nrf24.set_frequency(8).unwrap();
-    nrf24.set_auto_retransmit(15, 15).unwrap();
-    nrf24.set_rf(&DataRate::R2Mbps, 0).unwrap();
-    nrf24
-        .set_pipes_rx_enable(&[true, false, false, false, false, false])
+    // nrf24.set_frequency(8).unwrap();
+    // nrf24.set_auto_retransmit(15, 15).unwrap();
+    // nrf24.set_rf(&DataRate::R2Mbps, 0).unwrap();
+    // nrf24
+    //     .set_pipes_rx_enable(&[true, false, false, false, false, false])
+    //     .unwrap();
+    // nrf24
+    //     .set_auto_ack(&[true, false, false, false, false, false])
+    //     .unwrap();
+    // nrf24.set_pipes_rx_lengths(&[None; 6]).unwrap();
+    // nrf24.set_crc(CrcMode::TwoBytes).unwrap();
+    // nrf24.set_rx_addr(0, &b"fnord"[..]).unwrap();
+    // nrf24.set_tx_addr(&b"fnord"[..]).unwrap();
+    // nrf24.flush_rx().unwrap();
+    // nrf24.flush_tx().unwrap();
+
+    // https://github.com/burgerindividual/attiny-neopixel-receiver/blob/main/src/main.rs#L111
+    nrf24.set_rf(&DataRate::R250Kbps, 2).unwrap();
+    nrf24.set_frequency(13).unwrap();
+    nrf24.set_address_width(3).unwrap();
+    nrf24.set_crc(CrcMode::OneByte).unwrap();
+    nrf24.set_rx_addr(0, b"nrf").unwrap();
+    nrf24.set_auto_ack(&[false; PIPES_COUNT]).unwrap();
+    nrf24.set_pipes_rx_enable(&[true, false, false, false, false, false])
         .unwrap();
-    nrf24
-        .set_auto_ack(&[true, false, false, false, false, false])
-        .unwrap();
-    nrf24.set_pipes_rx_lengths(&[None; 6]).unwrap();
-    nrf24.set_crc(CrcMode::TwoBytes).unwrap();
-    nrf24.set_rx_addr(0, &b"fnord"[..]).unwrap();
-    nrf24.set_tx_addr(&b"fnord"[..]).unwrap();
-    nrf24.flush_rx().unwrap();
-    nrf24.flush_tx().unwrap();
+    nrf24.set_pipes_rx_lengths(&[None; PIPES_COUNT]).unwrap();
+    nrf24.set_interrupt_mask(false, true, true).unwrap();
+    nrf24.clear_interrupts().unwrap();
 
     // Print addresswidth
     println!("Address width is: {}", nrf24.get_address_width().unwrap());
 
     // Receive
     let mut nrf24 = nrf24.rx().unwrap(); //default configuration from example
-    let mut buff: [u8; 4] = [0; 4];
+    let mut buf: [u8; 4] = [0; 4];
     delay.delay_us(130u32);
 
     loop {
-        println!("Receiving data...");
-
-        if nrf24.can_read().is_ok() {
-            let payload = nrf24.read();
-            match payload {
-                Ok(p) => {
-                    //println!("Got payload = {}", p.as_ref().len());
-                    buff.copy_from_slice(p.as_ref());
-                    let num = u32::from_le_bytes(buff);
-
-                    println!("Got message = {:?}", num);
-                }
-                Err(_) => {
-                    println!("Could not read payload");
-                }
-            }
-        }
+       println!("Receiving data...");
+       if nrf24.can_read().is_ok() {
+           let payload = nrf24.read();
+           match payload {
+               Ok(p) => {
+                   //println!("Got payload = {}", p.as_ref());
+                   buf.copy_from_slice(p.as_ref());
+                   let num = u32::from_le_bytes(buf);
+                   println!("Got message = {:?}", num);
+               }
+               Err(_) => {
+                   println!("Could not read payload");
+               }
+           }
+       }
     }
+
+    // loop {
+    //     delay.delay_us(500u32);
+    //     if nrf24.can_read().is_ok() {
+    //         let payload = nrf24.read();
+    //         match payload {
+    //             Ok(p) => {
+    //                 println!("Got payload = {:?}", p.as_ref());
+    //             }
+    //             Err(_) => {
+    //                 println!("Could not read payload");
+    //             }
+    //         }
+    //     }
+    // }
+
+    // loop {
+    //     // process if RX ready interrupt is set
+    //     if nrf24.get_interrupts().unwrap().0 {
+    //         let payload = nrf24.read().unwrap();
+    //         nrf24.clear_interrupts().unwrap();
+
+    //         let decoded = core::str::from_utf8(&payload).unwrap();
+
+    //         // print the received data
+    //         println!("Received: {}", decoded);
+
+    //     }
+    //     // waiting for atleast 250μs ends the led line commands. we'll wait for 300μs
+    //     delay.delay_us(500u32);
+    //     //delay_cycles(4800);
+    //     // delay for 2ms, this is the maximum refresh speed of the led strip
+    //     // (this can be shorter)
+    //     // delay_cycles(16000 * 2);
+    // }
+
 }
