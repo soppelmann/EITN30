2d755de4b7c05d4499ed90146ab9b391cfa488bb
IvarNilsson <ivar.j.nilsson@gmail.com>
2024-04-05 09:45:09 +0200

    more testing

--- src/bin/rxloop.rs
+++ src/bin/rxloop.rs
@@ -4,12 +4,12 @@ use std::time::Duration;
 use nrf24l01::{OperatingMode, PALevel, RXConfig, NRF24L01};
 
 fn main() {
-    let config_1 = RXConfig {
-        channel: 108,
-        pa_level: PALevel::Min,
-        pipe0_address: *b"abcde",
-        ..Default::default()
-    };
+    //let config_1 = RXConfig {
+    //    channel: 108,
+    //    pa_level: PALevel::Min,
+    //    pipe0_address: *b"abcde",
+    //    ..Default::default()
+    //};
 
     let config_2 = RXConfig {
         channel: 108,
@@ -18,10 +18,10 @@ fn main() {
         ..Default::default()
     };
 
-    let mut device_1 = NRF24L01::new(17, 0, 0).unwrap();
+    //let mut device_1 = NRF24L01::new(17, 0, 0).unwrap();
     let mut device_2 = NRF24L01::new(27, 1, 1).unwrap();
     
-
+    
     device_2.configure(&OperatingMode::RX(config_2)).unwrap();
     device_2.listen().unwrap();
     loop {
