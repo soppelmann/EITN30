4bcca737365092d7f7d90f0cd129f5a9a20fc1f3
rpi25 <rpi25@eitn30.se>
2024-04-24 14:40:49 +0100

    Maybe this reduces the ipv4 check

--- src/rxloop.rs
+++ src/rxloop.rs
@@ -8,19 +8,15 @@ use tun2::platform::posix::Writer;
 pub fn rx_loop(mut device: NRF24L01, mut writer: Writer) {
     let mut buf = [0u8; BUFFER_SIZE];
     let mut end;
-    let mut emptybuf;
     loop {
         end = 0;
-        emptybuf = true;
-        while end <= 48 || emptybuf || packet::ip::v4::Packet::new(&buf[..end]).is_err() {
+        loop {
             if end + 96 >= BUFFER_SIZE {
                 end = 0;
-                emptybuf = true;
             }
             //sleep(Duration::from_micros(10));
             match device.data_available() {
                 Ok(true) => {
-                    emptybuf = false;
                     device
                         .read_all(|packet| {
                             //println!("Received {:?} bytes", packet.len());
@@ -39,8 +35,14 @@ pub fn rx_loop(mut device: NRF24L01, mut writer: Writer) {
                     println!("Error: {}", e);
                 }
             }
+
+            if end >= 48 {
+                if packet::ip::v4::Packet::new(&buf[..end]).is_err() {
+                    //println!("Writing {} bytes to interface", end);
+                    _ = writer.write(&buf[..end]).unwrap();
+                    break;
+                }
+            }
         }
-        //println!("Writing {} bytes to interface", end);
-        _ = writer.write(&buf[..end]).unwrap();
     }
 }
