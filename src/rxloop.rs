ffee97cdeebb0437e2f1f110e80a123e05a94e7a
getz <getz@sdf.org>
2024-05-15 16:02:26 +0200

    Revert rxloop changes

--- src/rxloop.rs
+++ src/rxloop.rs
@@ -9,13 +9,13 @@ use tun2::platform::posix::Writer;
 
 pub fn rx_loop(mut device: NRF24L01, writer: Arc<Mutex<Writer>>) {
     let mut buf = [0u8; BUFFER_SIZE];
+    let mut total_length: u16 = 3;
     let mut end;
+    let mut init;
     loop {
         end = 0;
-        while end <= 20
-            || (packet::ip::v4::Packet::new(&buf[..end]).is_err()
-                && packet::ip::v6::Packet::new(&buf[..end]).is_err())
-        {
+        init = true;
+        loop {
             sleep(Duration::from_micros(25));
             // Avoid buffer overflow in case of failure.
             if end + PACKET_SIZE * QUEUE_SIZE >= BUFFER_SIZE {
@@ -40,15 +40,29 @@ pub fn rx_loop(mut device: NRF24L01, writer: Arc<Mutex<Writer>>) {
                     println!("Error: {}", e);
                 }
             }
-        }
 
-        // Write if its not first iteration and we have received the amount
-        // of bytes that was listed in the IPv4 Total leangth field in the
-        // header. We need to create a mutex and massage rust but it works
-        let tun_writer_clone = writer.clone();
-        let _tun_writer = thread::spawn(move || {
-            let mut writer_ref = tun_writer_clone.lock().unwrap(); // Get a mutable reference
-            _ = writer_ref.write(&buf[..end]).unwrap();
-        });
+            // Better ipv4 is_err(check)
+            // We find out the Total Length of the packet using bytes 2 and 3.
+            // see: https://en.wikipedia.org/wiki/Internet_Protocol_version_4#Total_Length
+            if init && end == 32 {
+                // This might break if receiving a packet smaller than 32 bytes
+                let length_slice = &buf[2..4];
+                total_length = u16::from_be_bytes([length_slice[0], length_slice[1]]);
+                init = false;
+            }
+
+            // Write if its not first iteration and we have received the amount
+            // of bytes that was listed in the IPv4 Total leangth field in the
+            // header. We need to create a mutex and massage rust but it works
+            if !init && end == total_length as usize {
+                let tun_writer_clone = writer.clone();
+                let _tun_writer = thread::spawn(move || {
+                    let mut writer_ref = tun_writer_clone.lock().unwrap(); // Get a mutable reference
+                    _ = writer_ref.write(&buf[..end]).unwrap();
+                });
+
+                break;
+            }
+        }
     }
 }
